0a4243aaeed1f6e493cd6735506d3a292eab415a
Arkadi Sahakyan <arkadijsaakyan3@gmail.com>
2024-11-26 20:21:59 +0300

    Initial commit

---
+++ consumer/src/main/java/arkadisahakyan/Consumer.java
@@ -0,0 +1,36 @@
+package arkadisahakyan;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeoutException;
+
+public class Consumer
+{
+    private static final String QUEUE_NAME = "hello";
+    private static final Integer MAX_MESSAGES_COUNT = 100;
+
+    public static void main( String[] args ) throws IOException, TimeoutException, InterruptedException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        try (Connection connection = connectionFactory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+            CountDownLatch countDownLatch = new CountDownLatch(MAX_MESSAGES_COUNT);
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+                countDownLatch.countDown();
+            };
+            String consumer = channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+            countDownLatch.await();
+            channel.basicCancel(consumer);
+        }
+    }
+}
---
+++ publisher/src/main/java/arkadisahakyan/Publisher.java
@@ -0,0 +1,32 @@
+package arkadisahakyan;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.TimeoutException;
+
+public class Publisher
+{
+    private static final String QUEUE_NAME = "hello";
+    private static final Integer MAX_MESSAGES_COUNT = 100;
+
+    public static void main( String[] args ) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        try (Connection connection = connectionFactory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            System.out.println(" [*] Send your message. To exit press CTRL+C");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            for (int i = 0; i < MAX_MESSAGES_COUNT; i++) {
+                String message = reader.readLine();
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+                System.out.println(" [x] Sent '" + message + "'");
+            }
+        }
+    }
+}
