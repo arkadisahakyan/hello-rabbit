b4eb36da7049e3d98524ca86da927755a512b4f3
Arkadi Sahakyan <arkadijsaakyan3@gmail.com>
2024-12-01 15:56:18 +0300

    Simplify publisher-consumer program for Docker

--- publisher/src/main/java/arkadisahakyan/Publisher.java
+++ publisher/src/main/java/arkadisahakyan/Publisher.java
@@ -4,9 +4,7 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.concurrent.TimeoutException;
 
 public class Publisher
@@ -20,15 +18,9 @@ public class Publisher
         try (Connection connection = connectionFactory.newConnection();
              Channel channel = connection.createChannel()) {
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            System.out.println(" [*] Send your message. To exit press CTRL+C");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String message;
-            for (int i = 0; i < MAX_MESSAGES_COUNT; i++) {
-                if ((message = reader.readLine()) != null) {
-                    channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-                    System.out.println(" [x] Sent '" + message + "'");
-                }
-            }
+            String message = "Hello, world!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
         }
     }
 }
